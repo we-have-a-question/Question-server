56cc6d25bc28df7978e6d4cdb3a117a90544f95d
sunnyday0206 <sunme8412@gmail.com>
2026-04-16 12:03:01 +0900

    init: project setting

---
+++ src/main/java/org/example/questionserver/QuestionServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.questionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/questionserver/QuestionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.questionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
